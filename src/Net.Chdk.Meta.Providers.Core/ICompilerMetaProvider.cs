df7df48b7d7b6eae2b77b1d731c4e025d3f3223a
Dmitry Shechtman <damnian@gmail.com>
2017-07-03 01:49:09 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers.Core/ICompilerMetaProvider.cs
@@ -0,0 +1,9 @@
+﻿using Net.Chdk.Model.Software;
+
+namespace Net.Chdk.Meta.Providers
+{
+    public interface ICompilerMetaProvider
+    {
+        SoftwareCompilerInfo GetCompiler(SoftwareInfo software);
+    }
+}
---
+++ src/Net.Chdk.Meta.Providers.Core/IProductMetaProvider.cs
@@ -0,0 +1,10 @@
+﻿using Net.Chdk.Model.Software;
+using System;
+
+namespace Net.Chdk.Meta.Providers
+{
+    public interface IProductMetaProvider
+    {
+        SoftwareProductInfo GetProduct(string name, DateTime created);
+    }
+}
